6798c1cfb2008b71330bd23b43c072bd3cab3ded
Peter Siman <peter.siman@wandera.com>
2019-03-07 15:04:51 +0100

    Port configuration

--- main.go
+++ main.go
@@ -88,11 +88,11 @@ func (p *proxy) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	var addr = flag.String("addr", "127.0.0.1:8080", "The addr of the application.")
+	var addr = flag.String("addr", ":" + os.Getenv("PORT"), "The addr of the application.")
 	flag.Parse()
-	
+
 	handler := &proxy{}
-	
+
 	log.Println("Starting proxy server on", *addr)
 	if err := http.ListenAndServe(*addr, handler); err != nil {
 		log.Fatal("ListenAndServe:", err)
